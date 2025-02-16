82cb07aea2fdec2ab225452974037d9f3b4d59fa
AsilbekAbdusattorov <abdusattorovasilbek278@gmail.com>
2025-02-16 16:02:27 +0500

    Add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0', // Tarmoqdan tashqarida ham ishlashini ta'minlash
   },
   preview: {
     allowedHosts: ['savdo-sotiq-2.onrender.com', 'localhost', '127.0.0.1'],
   },
-}
+})
